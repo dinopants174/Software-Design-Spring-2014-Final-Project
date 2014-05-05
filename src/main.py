b8b4c4891ab44a92d493c3e899dbca81a7331d21
themythicaldrago <ryan.louie@students.olin.edu>
2014-05-05 02:54:31 -0400

    added plotting of segments and components during the process for debugging purposes.  classifier works better. problem is mainly in component_finder where it finds 3 instead of 2 components

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import os
 
+import matplotlib.pyplot as plt
+
 from sklearn.externals import joblib
 
 import ImageCropper
@@ -8,11 +10,12 @@ from ComponentClassifier import ComponentClassifier
 
 class System:
 
-	def __init__(self, clf_fn='randomforest_rawpix_nbins6.pkl', nbins=6):
+	def __init__(self, clf_fn, nbins, scaler_fn):
 		self.data_dir = os.path.join(os.path.abspath('../'),'data/')
 		self.images_dir = os.path.join(self.data_dir, 'TestImages/')
 		self.clf = joblib.load(os.path.join(self.data_dir, clf_fn))
 		self.nbins = nbins
+		self.scaler = joblib.load(os.path.join(self.data_dir, scaler_fn))
 
 	def run(self, image):
 		"""
@@ -63,13 +66,18 @@ class System:
 		
 		for segment in segments:
 			# FInd component along a segment and crop around them
+			plt.imshow(segment.image, cmap='gray')
+			plt.show()
 			component_images = DiagramAnalyzer.component_finder(segment)
 
 			# Resistor vs Capacitor Component Classifier
 			component_id_list = ComponentClassifier.predict(
 									component_images, 
 									self.clf, 
-									self.nbins)
+									self.nbins,
+									self.scaler)
+
+			print component_id_list
 			segment.finding_components(component_id_list)
 
 			# Draw a beautified digital equivalent of the segment
@@ -80,7 +88,9 @@ class System:
 
 
 if __name__ == '__main__':
-	SmarterBoard = System()
+	SmarterBoard = System(clf_fn='SVC_scaled_moments_nbins23_INTERP_CUBIC.pkl',
+						  nbins=23, 
+						  scaler_fn='Scaler_nbins23.pkl')
 
 	fn = 'intersection-test.jpg'
 	image = ImageCropper.open_file(os.path.join(SmarterBoard.images_dir, fn))
